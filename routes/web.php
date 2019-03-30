2d274251ccbbaa8c797afbb73d3574a8c41c3337
zloyleva <zloyleva@gmail.com>
2019-03-30 20:44:09 +0200

    add Catalog routes

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,22 @@
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+| These routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group.
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::prefix("catalog")->group(function(){
+    Route::get('/',"CatalogController@index")->name("catalog");
+    Route::get('/{slug}',"CatalogController@catalogSlug")->name("catalogSlug")->where('slug','[A-Za-z0-9-]+');
+});
+
 Auth::routes();
 
 Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
+
+// Temp routes
+Route::get('/price', 'PageController@readPrice')->name('readPrice');
