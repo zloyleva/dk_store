5d075c78d2c2eeeb1272750fe82fdb9561aa2868
zloyleva <zloyleva@gmail.com>
2019-03-31 22:42:55 +0300

    add CartItem Model

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -3,12 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CartItem extends Model
 {
     protected $fillable = ['user_id', 'product_id', 'count'];
 
-    public function product(){
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product():BelongsTo
+    {
         return $this->belongsTo(Product::class,"product_id", "id");
     }
 
--- database/migrations/2019_03_31_152152_create_cart_items_table.php
+++ database/migrations/2019_03_31_152152_create_cart_items_table.php
@@ -8,7 +8,6 @@ class CreateCartItemsTable extends Migration
 {
     /**
      * Run the migrations.
-     *
      * @return void
      */
     public function up()
